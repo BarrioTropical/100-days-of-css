bd9a5d0a97ac9cd8f55629b8fe9576ea546c9bef
Barrio Tropical <barriotropical@gmail.com>
2024-06-17 22:12:51 +0200

    doc
    Signed-off-by: Barrio Tropical <barriotropical@gmail.com>

---
+++ Powered Circular Text Slider Scroll Animation/data.js
@@ -0,0 +1,11 @@
+export const interiors = [
+    { id: 1, name: "Lunar Horizon Lounge"},
+    { id: 2, name: "Martian Red Quarters"},
+    { id: 3, name: "Padova"},
+    { id: 4, name: "Barcelona"},
+    { id: 5, name: "Eixample"},
+    { id: 6, name: "Gracia"},
+    { id: 7, name: "Barceloneta"},
+    { id: 8, name: "Raval"},
+    { id: 9, name: "Barri Gothic"},
+]
